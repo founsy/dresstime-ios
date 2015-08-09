c82274acae49a93b5876d2c56617e8b753509e72
F Langlet <flanglet@drez.io>
2015-08-09 11:17:39 +0200

    Add border to sub type selection

--- DressTime/TypeTableViewCell.swift
+++ DressTime/TypeTableViewCell.swift
@@ -1,4 +1,4 @@
-//
+        //
 //  TypeTableViewCell.swift
 //  DressTime
 //
@@ -9,9 +9,45 @@
 import Foundation
 import UIKit
 
+class IndexedCollectionView: UICollectionView {
+            
+    var indexPath: NSIndexPath!
+            
+    override init(frame: CGRect, collectionViewLayout layout: UICollectionViewLayout) {
+        super.init(frame: frame, collectionViewLayout: layout)
+    }
+            
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+}
+        
 class TypeTableViewCell : UITableViewCell {
 
     @IBOutlet var bgImage: UIImageView!
+    var collectionView: IndexedCollectionView!
+    private let kCellReuse : String = "SubTypeCell"
+    private let collectionCellWidth:CGFloat = 114.0
+    var collectionWidth: CGFloat!
+    var currentSection: Int!
+    var isLoaded = false
+    
+    var data: [String]! {
+        didSet{
+            self.isLoaded = true
+        }
+    }
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?){
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        initialize()
+    }
+
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        initialize()
+    }
+    
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -23,4 +59,63 @@ class TypeTableViewCell : UITableViewCell {
         
         // Configure the view for the selected state
     }
+    
+    func initialize(){
+        var collectionLayout = UICollectionViewFlowLayout()
+        collectionLayout.sectionInset = UIEdgeInsetsMake(0, 0, 0, 0)
+        collectionLayout.itemSize = CGSizeMake(self.collectionCellWidth, 90);
+        collectionLayout.scrollDirection = UICollectionViewScrollDirection.Vertical
+        
+        collectionView = IndexedCollectionView(frame: CGRectZero, collectionViewLayout: collectionLayout)
+        collectionView.showsVerticalScrollIndicator = false
+        collectionView.backgroundColor = UIColor.clearColor()
+        
+        //collectionView.dataSource = self
+        //collectionView.delegate = self
+        
+        var customCell = UINib(nibName: "SubTypeCell", bundle: nil)
+        self.collectionView.registerNib(customCell, forCellWithReuseIdentifier: kCellReuse)
+        self.contentView.addSubview(self.collectionView)
+        self.collectionView.hidden = true
+    }
+    
+    func showCollectionView(){
+        self.collectionView.reloadData()
+        self.collectionView.hidden = false
+    }
+    
+    func hideCollectionView(){
+        self.collectionView.hidden = true
+    }
+    
+    func calculateCollectionViewHeight() -> CGFloat {
+        let cellHeight = 100.0
+        let height = round(Double(data.count)/2.0) * cellHeight
+        return CGFloat(height)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        var collectionViewWidth = CGFloat(self.collectionCellWidth*2)
+        let frame = self.contentView.bounds
+        var margin = CGFloat((frame.width - collectionViewWidth)/2)
+        
+        self.collectionView.frame = CGRectMake(margin, 10, CGFloat(self.collectionCellWidth*2), calculateCollectionViewHeight())
+    }
+    
+    func setCollectionViewDataSourceDelegate(dataSourceDelegate delegate: protocol<UICollectionViewDelegate,UICollectionViewDataSource>, index: NSInteger) {
+        self.collectionView.dataSource = delegate
+        self.collectionView.delegate = delegate
+        self.collectionView.tag = index
+        self.collectionView.reloadData()
+    }
+    
+    func setCollectionViewDataSourceDelegate(dataSourceDelegate delegate: protocol<UICollectionViewDelegate,UICollectionViewDataSource>, indexPath: NSIndexPath) {
+        self.collectionView.dataSource = delegate
+        self.collectionView.delegate = delegate
+        self.collectionView.indexPath = indexPath
+        self.collectionView.tag = indexPath.section
+        self.collectionView.reloadData()
+    }
+
 }
--- DressTime/TypeViewController.swift
+++ DressTime/TypeViewController.swift
@@ -26,33 +26,21 @@ class TypeViewController: UIViewController {
     private let barSize : CGFloat = 44.0
     private let kCellReuse : String = "SubTypeCell"
     private let kCellType : String = "TypeCell"
-    private var collectionView : UICollectionView = UICollectionView(frame: CGRectZero, collectionViewLayout: UICollectionViewFlowLayout())   // Initialization
     
-    private var currentSection = -1
-    
+    private var currentSection = 0
+    private let collectionCellWidth = 114
+    private var isLoaded = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         arrayForBool = ["0","0", "0"]
         sectionTitleArray = ["Maille","Top", "Pants"]
-        
-        var tmp1 : NSArray = ["jumper-fin","jumper-epais ","cardigan","sweater"]
-        var string1 = sectionTitleArray .objectAtIndex(0) as? String
-        [sectionContentDict .setValue(tmp1, forKey:string1! )]
-        
-        var tmp2 : NSArray = ["tshirt", "shirt", "shirt-sleeve", "polo","polo-sleeve"]
-        string1 = sectionTitleArray .objectAtIndex(1) as? String
-        [sectionContentDict .setValue(tmp2, forKey:string1! )]
-        
-        var tmp3 : NSArray = ["jeans", "jeans-slim", "trousers-pleated", "trousers-suit", "chinos", "trousers-regular", "trousers", "trousers-slim", "bermuda", "short"]
-        string1 = sectionTitleArray .objectAtIndex(2) as? String
-        [sectionContentDict .setValue(tmp3, forKey:string1! )]
-        
     }
 }
 
 extension TypeViewController: UITableViewDelegate{
+    
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         println("You selected cell #\(indexPath.row)!")
         let oldRow = self.currentSection
@@ -75,7 +63,7 @@ extension TypeViewController: UITableViewDelegate{
             
         arrayForBool.replaceObjectAtIndex(indexPath.row, withObject: collapsed)
         self.tableView.reloadRowsAtIndexPaths([indexPath], withRowAnimation: UITableViewRowAnimation.Fade)
-        self.collectionView.reloadData()
+        self.tableView.scrollToRowAtIndexPath(indexPath, atScrollPosition: UITableViewScrollPosition.Top, animated: true)
     }
 }
 
@@ -86,7 +74,7 @@ extension TypeViewController: UITableViewDataSource {
     }
     
     func calculateCollectionViewHeight() -> CGFloat {
-        let cellHeight = 110.0
+        let cellHeight = 100.0
         
         var height = 0.0
         switch(self.currentSection) {
@@ -112,53 +100,89 @@ extension TypeViewController: UITableViewDataSource {
         }
 
     }
+
+    func tableView(tableView: UITableView, willDisplayCell cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath) {
+        NSLog("willDisplayCell \(indexPath.row)")
+        var currentCell = cell as! TypeTableViewCell
+        currentCell.setCollectionViewDataSourceDelegate(dataSourceDelegate: self, index: indexPath.row)
+    
+        if (arrayForBool.objectAtIndex(indexPath.row).boolValue as Bool){
+            currentCell.bgImage.image = UIImage(named: "\(bgType[indexPath.row])Full")
+            currentCell.collectionWidth = self.tableView.bounds.width
+            currentCell.showCollectionView()
+        } else {
+            var height = self.tableView.bounds.height;
+        
+            //Resize cell
+            currentCell.contentView.frame = CGRectMake(0, 0, tableView.frame.size.width, round(height*0.33333))
+            currentCell.bgImage.image = UIImage(named: bgType[indexPath.row])
+            currentCell.hideCollectionView()
+        }
+    }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
-        var cell = self.tableView.dequeueReusableCellWithIdentifier(kCellType) as! TypeTableViewCell
-        
-        var height = self.tableView.bounds.height;
-        //Resize cell
-        cell.contentView.frame = CGRectMake(0, 0, tableView.frame.size.width, round(height*0.33333))
-        
-        if (arrayForBool.objectAtIndex(indexPath.row).boolValue as Bool){
-            cell.bgImage.image = UIImage(named: "\(bgType[indexPath.row])Full")
-            var layout = UICollectionViewFlowLayout()
-            // Collection
-            self.collectionView.delegate = self     // delegate  :  UICollectionViewDelegate
-            self.collectionView.dataSource = self   // datasource  : UICollectionViewDataSource
-            self.collectionView.backgroundColor = UIColor.clearColor()
-            
-            self.collectionView.frame = CGRectMake(0, 0, tableView.frame.size.width, calculateCollectionViewHeight())
-            
-            // Register parts(header and cell
-            var customCell = UINib(nibName: "SubTypeCell", bundle: nil)
-            self.collectionView.registerNib(customCell, forCellWithReuseIdentifier: kCellReuse)
-            cell.contentView.addSubview(self.collectionView)
-            
-        } else {
-            cell.bgImage.image = UIImage(named: bgType[indexPath.row])
-        }
-        
+        NSLog("cellForRowAtIndexPath \(indexPath.row)")
+        var cell = self.tableView.dequeueReusableCellWithIdentifier(kCellType, forIndexPath: indexPath) as! TypeTableViewCell
+        cell.data = getData(self.currentSection)
+    
         return cell
     }
 }
 
 extension TypeViewController : UICollectionViewDataSource {
-    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
-        var cell : CustomSubTypeViewCell = collectionView.dequeueReusableCellWithReuseIdentifier(kCellReuse, forIndexPath: indexPath) as! CustomSubTypeViewCell
-        println(self.currentSection)
-        switch(self.currentSection) {
-        case 0:
-            cell.label.text = labelsSubMaille[indexPath.row]
-        case 1:
-            cell.label.text = labelsSubTop[indexPath.row]
-        case 2:
-            cell.label.text = labelsSubPants[indexPath.row]
-        default:
-            cell.label.text = ""
-            break
+    
+    func collectionView(collectionView: UICollectionView, willDisplayCell cell: UICollectionViewCell, forItemAtIndexPath indexPath: NSIndexPath) {
+        var collectionCell = cell as! CustomSubTypeViewCell
+        
+        if ((indexPath.row%2) == 0){
+            collectionCell = addRightBorder(collectionCell)
         }
-        return cell    //Create UICollectionViewCell
+        var count:Int = 0
+        if (collectionView.numberOfItemsInSection(0)%2 == 0){
+            count = (collectionView.numberOfItemsInSection(0)/2)-1
+        } else {
+            count = (collectionView.numberOfItemsInSection(0)/2)
+            
+        }
+        let currentNb:Int = indexPath.row/2
+        if (currentNb < count){
+            collectionCell = addBottomBorder(collectionCell)
+        }
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        var cell = collectionView.dequeueReusableCellWithReuseIdentifier(kCellReuse, forIndexPath: indexPath) as! CustomSubTypeViewCell
+        cell.label.text = getData(self.currentSection)![indexPath.row]
+        cell.contentView.viewWithTag(100)?.removeFromSuperview()
+        cell.contentView.viewWithTag(101)?.removeFromSuperview()
+
+        return cell
+    }
+    
+    func addRightBorder(cell: CustomSubTypeViewCell) -> CustomSubTypeViewCell {
+        var mainViewSize = cell.bounds.size
+        var borderWidth:CGFloat = 1.0
+        var borderColor = UIColor.whiteColor()
+        var rightView = UIView(frame: CGRectMake(CGFloat(mainViewSize.width - borderWidth), 0, borderWidth, mainViewSize.height))
+        rightView.tag = 100
+        rightView.backgroundColor = UIColor.whiteColor()
+        
+        cell.contentView.addSubview(rightView)
+        
+        return cell
+    }
+    
+    func addBottomBorder(cell: CustomSubTypeViewCell) -> CustomSubTypeViewCell {
+        var mainViewSize = cell.bounds.size
+        var borderWidth:CGFloat = 1.0
+        var borderColor = UIColor.whiteColor()
+        var rightView = UIView(frame: CGRectMake(0, CGFloat(mainViewSize.height - borderWidth), mainViewSize.width, borderWidth))
+        rightView.tag = 101
+        rightView.backgroundColor = UIColor.whiteColor()
+        
+        cell.contentView.addSubview(rightView)
+        
+        return cell
     }
     
     func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
@@ -166,25 +190,34 @@ extension TypeViewController : UICollectionViewDataSource {
     }
     
     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        var res = 0
-        switch(self.currentSection) {
-        case 0:
-            res = labelsSubMaille.count // Number of cell per section(section 0)
-        case 1:
-            res = labelsSubTop.count // Number of cell per section(section 0)
-        case 2:
-            res = labelsSubPants.count // Number of cell per section(section 0)
-        default:
-            res = 0
-            break
+        if let datas = getData(self.currentSection) {
+            return datas.count
+        } else {
+            return 0
         }
-        return res
+    }
+    
+    func getData(section: Int) -> [String]?{
+        switch(section){
+        case 0:
+            return self.labelsSubMaille
+        case 1:
+            return self.labelsSubTop
+        case 2:
+            return self.labelsSubPants
+        default:
+            return nil
+        }
     }
 }
 
 extension TypeViewController : UICollectionViewDelegate {
+    
     func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
         collectionView.deselectItemAtIndexPath(indexPath, animated: false)
+        var cell = collectionView.cellForItemAtIndexPath(indexPath)
+        NSLog("Nbr subviews:  \(cell?.contentView.subviews.count)")
+        NSLog("\(indexPath.row)")
         // Select operation
         println("tapped on collection")
         var captureView = CameraOverlayView()
@@ -208,45 +241,41 @@ extension TypeViewController : UICollectionViewDelegate {
     }
     
     func getSubType(type: Int, subType: Int) -> String {
-        switch(type){
-        case 0:
-            return labelsSubMaille[subType]
-        case 1:
-            return labelsSubTop[subType]
-        case 2:
-            return labelsSubPants[subType]
-        default:
-            return ""
-        }
+        return getData(type)![subType]
         
-    }
-}
-
-extension TypeViewController : CameraOverlayViewDelegate {
-    func CameraOverlayViewResult(resultCapture: [String: AnyObject]) {
-        var dal = ClothesDAL()
-        let clotheId = NSUUID().UUIDString
-        dal.save(clotheId, partnerId: resultCapture["clothe_partnerid"] as! NSNumber, partnerName: resultCapture["clothe_partnerName"] as! String, type: resultCapture["clothe_type"] as! String, subType: resultCapture["clothe_subtype"] as! String, name: resultCapture["clothe_name"] as! String, isUnis: resultCapture["clothe_isUnis"] as! Bool, pattern: resultCapture["clothe_pattern"] as! String, cut: resultCapture["clothe_cut"] as! String, image: resultCapture["clothe_image"] as! NSData, colors: resultCapture["clothe_colors"] as! String)
-        //self.collection = clothesDAL.fetch(self.type)
-        //self.collectionView.reloadData()
-        
-        DressTimeService.saveClothe(SharedData.sharedInstance.currentUserId!, clotheId: clotheId, dressingCompleted: { (succeeded: Bool, msg: [[String: AnyObject]]) -> () in
-            //println(msg)
-        })
     }
 }
 
 extension TypeViewController: UICollectionViewDelegateFlowLayout {
+    
     func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
-        return CGSize(width: 150, height: 90) // The size of one cell
+        return CGSize(width: self.collectionCellWidth, height: 90) // The size of one cell
     }
     
     func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAtIndex section: Int) -> UIEdgeInsets {
-        let frame : CGRect = self.view.frame
-        let margin  = (frame.width - 90 * 3) / 6.0
-        return UIEdgeInsetsMake(10, margin, 10, margin) // margin between cells
+        return UIEdgeInsetsMake(0, 0, 0, 0)
     }
+    
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAtIndex section: Int) -> CGFloat {
+        return 0.0
+    }
+    
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAtIndex section: Int) -> CGFloat {
+        return 0.0
+    }
+}
 
+extension TypeViewController : CameraOverlayViewDelegate {
+    
+    func CameraOverlayViewResult(resultCapture: [String: AnyObject]) {
+        var dal = ClothesDAL()
+        let clotheId = NSUUID().UUIDString
+        dal.save(clotheId, partnerId: resultCapture["clothe_partnerid"] as! NSNumber, partnerName: resultCapture["clothe_partnerName"] as! String, type: resultCapture["clothe_type"] as! String, subType: resultCapture["clothe_subtype"] as! String, name: resultCapture["clothe_name"] as! String, isUnis: resultCapture["clothe_isUnis"] as! Bool, pattern: resultCapture["clothe_pattern"] as! String, cut: resultCapture["clothe_cut"] as! String, image: resultCapture["clothe_image"] as! NSData, colors: resultCapture["clothe_colors"] as! String)
+
+        DressTimeService.saveClothe(SharedData.sharedInstance.currentUserId!, clotheId: clotheId, dressingCompleted: { (succeeded: Bool, msg: [[String: AnyObject]]) -> () in
+            //println(msg)
+        })
+    }
 }
 
 extension UIView {
